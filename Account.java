d61ba75633e8d145f739a1139ea2204561842801
Efe Havza <efehavza@Efes-MacBook-Pro.local>
2018-01-10 01:16:41 +0300

    Added Account class and Unit test

---
+++ Account.java
@@ -0,0 +1,77 @@
+package constructors_challange;
+
+public class Account {
+    public enum Type {
+        WITHDRAWAL,
+        DEPOSIT,
+        UNKNOWN
+
+    }
+    private String name;
+    private String surname;
+    private long accountNumber;
+    private String emailAddress;
+    private double balance;
+
+    public Account(String name, String surname, long accountNumber, String emailAddress) {
+        this.name = name;
+        this.surname = surname;
+        this.accountNumber = accountNumber;
+        this.emailAddress = emailAddress;
+        this.balance = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public long getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public String getEmailAddress() {
+        return this.emailAddress;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setName(String nm) {
+        this.name = nm;
+    }
+
+    public void setSurname(String sn) {
+        this.surname = sn;
+    }
+
+    public void setAccountNumber(Long accNum) {
+        this.accountNumber = accNum;
+    }
+
+    public void setEmailAddress(String addr) {
+        this.emailAddress = addr;
+    }
+
+    public void setBalance(Type t, double amount) throws Exception {
+        switch(t) {
+            case DEPOSIT:
+                this.balance = this.balance + amount;
+                break;
+            case WITHDRAWAL:
+                if(balance < amount) {
+                    throw new Exception("Insufficient funds.");
+                } else {
+                    this.balance = this.balance - amount;
+                }
+                break;
+            default:
+                throw new Exception("Invalid type of Transaction.");
+        }
+    }
+
+}
---
+++ AccountTest.java
@@ -0,0 +1,51 @@
+package constructors_challange;
+
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args) throws Exception{
+        Account jane = new Account("Jane","Appleseed", 00000001l, "jane.appleseed@apple.com");
+        jane.setBalance(Account.Type.DEPOSIT,200); // set the fucking balance
+        Account.Type t = Account.Type.UNKNOWN;
+        String decision;
+        String select;
+        double amount;
+        String type;
+        Scanner in;
+
+
+        while (true) {
+            System.out.println("Name: " + jane.getName() + "\n" + "Surname: " + jane.getSurname() + "\n" + "Account Number: " + jane.getAccountNumber() + "\n" + "E-mail address: " + jane.getEmailAddress());
+
+            in = new Scanner(System.in);
+            System.out.println("Withdraw, deposit or balance ? (type withdraw, deposit or balance) ");
+            type = in.next();
+
+            if (type.toLowerCase().compareTo(("balance")) == 0) {
+                System.out.println("Your balance: " + jane.getBalance() + "$");
+            } else {
+                if (type.toLowerCase().compareTo("withdraw") == 0) t = Account.Type.WITHDRAWAL;
+                if (type.toLowerCase().compareTo("deposit") == 0) t = Account.Type.DEPOSIT;
+                System.out.println("Set the amount: ");
+                amount = in.nextDouble();
+                jane.setBalance(t, amount);
+                System.out.println("Selected Amount: " + amount + "$");
+                System.out.println("Your balance: " + jane.getBalance() + "$");
+
+            }
+
+            System.out.println("Do you wish to continue Yes/No ?");
+            select = in.next();
+            decision = select.toLowerCase();
+
+            if (decision.equals("yes"))
+            {
+                continue;
+            }
+            else if(decision.equals("no"))
+            {
+                break;
+            }
+        }
+    }
+}
